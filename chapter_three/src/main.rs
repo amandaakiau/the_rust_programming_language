82b1143d3e4adf3b8c224198c0b37547ba135154
Amanda Akiau <amanda96akiau@gmail.com>
2024-04-10 20:47:18 -0300

    chapter three init

---
+++ chapter_three/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // mut serve para indicar que a variável em questão é mutavel
+    let mut x = 5;
+    let y = 10;
+    println!("The value of x is {x} and y is {y}");
+    x = 6;
+    println!("Now the value of x is {x} and y is {y}")
+}
